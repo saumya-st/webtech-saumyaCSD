ceac6541b2c31928a66b2cfc8bc2b15eee0e1d03
saumya-st <saumyacodes02@gmail.com>
2026-02-13 15:03:28 +0530

    update

--- frii.js
+++ frii.js
@@ -12,3 +12,36 @@ console.log("Task done");
 } , 0);
 for(let i = 0;i<50;i++){
 console.log("end");}
+
+
+function greet(name){
+    console.log("hello"+ name);
+}
+function processUserInput(callback){
+    let name = "saumya";
+    callback(name);
+}
+processUserInput(greet);
+
+
+
+
+console.log("start");
+setTimeout(function(){
+    console.log("Inside timeout");
+},20000);
+console.log("end");
+
+
+//callback hell problem
+
+//future result - promise
+
+
+
+let promise = new Promise(function (resolev,reject){
+    let success = true;
+    if(success)
+{
+    
+}}
